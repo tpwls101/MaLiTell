33849fe0f892d3607c2c0e532919a9373b788655
jaehyeon <hjaehyeon98@gmail.com>
2024-01-23 17:28:20 +0900

    chore: 소셜로그인 및 시큐리티 완료

--- Backend/Malitell/src/main/java/com/ssafy/malitell/repository/UserRepository.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/repository/UserRepository.java
@@ -4,8 +4,7 @@ import com.ssafy.malitell.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Boolean existsByUserId(String userId);
     User findByUserId(String userId);
-
 }
---
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/service/implement/AuthServiceImplement.java
@@ -0,0 +1,131 @@
+package com.ssafy.malitell.service.implement;
+
+
+import com.ssafy.malitell.common.CertificationNumber;
+import com.ssafy.malitell.domain.CertificationEntity;
+import com.ssafy.malitell.domain.User;
+import com.ssafy.malitell.dto.request.auth.*;
+import com.ssafy.malitell.dto.response.ResponseDto;
+import com.ssafy.malitell.dto.response.auth.*;
+import com.ssafy.malitell.jwt.JWTUtil;
+import com.ssafy.malitell.repository.CertificationRepository;
+import com.ssafy.malitell.repository.UserRepository;
+import com.ssafy.malitell.service.AuthService;
+import com.ssafy.malitell.util.EmailUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImplement implements AuthService {
+
+    private final UserRepository userRepository;
+    private final CertificationRepository certificationRepository;
+
+    private final JWTUtil jwtUtil;
+    private final EmailUtil emailUtil;
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public ResponseEntity<? super IdCheckResponseDto> idCheck(IdCheckRequestDto dto) {
+        try {
+
+            String userId = dto.getId();
+            boolean isExistId = userRepository.existsByUserId(userId);
+
+            if (isExistId) return IdCheckResponseDto.duplicated();
+
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return IdCheckResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super EmailCertificationResponseDto> emailCertification(EmailCertificationRequestDto dto) {
+        try {
+
+            String userId = dto.getId();
+            String email = dto.getEmail();
+            boolean isExistId = userRepository.existsByUserId(userId);
+            if (isExistId) return EmailCertificationResponseDto.duplicateId();
+
+            String certificationNumber = CertificationNumber.getCertificationNumber();
+
+            boolean isSuccessed = emailUtil.sendCertificationMail(email, certificationNumber);
+            if (!isSuccessed) return EmailCertificationResponseDto.mailSendFail();
+
+            CertificationEntity certificationEntity = new CertificationEntity(userId, email, certificationNumber);
+            certificationRepository.save(certificationEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return EmailCertificationResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super CheckCertificationResponseDto> checkCertification(CheckCertificationRequestDto dto) {
+        try {
+
+            String userId = dto.getId();
+            String email = dto.getEmail();
+            String certificationNumber = dto.getCertificationNumber();
+
+            CertificationEntity certificationEntity = certificationRepository.findByUserId(userId);
+            if (certificationEntity == null) {
+                return CheckCertificationResponseDto.certificationFail();
+            }
+
+            boolean isMatched = certificationEntity.getEmail().equals(email) && certificationEntity.getCertificationNumber().equals(certificationNumber);
+            if (!isMatched) return CheckCertificationResponseDto.certificationFail();
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return CheckCertificationResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto) {
+        try {
+            String userId = dto.getId();
+            boolean isExistId = userRepository.existsByUserId(userId);
+            if (isExistId) {
+                return SignUpResponseDto.duplicated();
+            }
+
+            String email = dto.getEmail();
+            String certificationNumber = dto.getCertificationNumber();
+            CertificationEntity certificationEntity = certificationRepository.findByUserId(userId);
+            boolean isMatched = certificationEntity.getEmail().equals(email) && certificationEntity.getCertificationNumber().equals(certificationNumber);
+            if (!isMatched) {
+                return SignUpResponseDto.certificationFail();
+            }
+
+            String password = dto.getPassword();
+            String encodePassword = passwordEncoder.encode(password);
+            dto.setPassword(encodePassword);
+            User user = new User(dto);
+            userRepository.save(user);
+
+            certificationRepository.deleteByUserId(userId);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return SignUpResponseDto.success();
+    }
+}
