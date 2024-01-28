293025ec0c796e35fc297c94dd0ca59314b27e35
kn9012 <kn9012@naver.com>
2024-01-28 11:34:29 +0900

    Feat: Redis 설정 및 연결 확인 TC 작성

---
+++ Backend/Malitell/src/test/java/com/ssafy/malitell/ChatMessageTest.java
@@ -0,0 +1,26 @@
+package com.ssafy.malitell;
+
+import com.ssafy.malitell.domain.chatMessage.ChatMessage;
+import com.ssafy.malitell.repository.RedisRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static java.time.LocalTime.now;
+
+
+@SpringBootTest
+public class ChatMessageTest {
+    @Autowired
+    private RedisRepository redisRepository;
+
+    @Test
+    void test() {
+        ChatMessage chatMessage = new ChatMessage(1L, 1, 1, "hello!", now(), true);
+        redisRepository.save(chatMessage);
+        redisRepository.findById(chatMessage.getChatMessageSeq());
+        redisRepository.count();
+        redisRepository.delete(chatMessage);
+    }
+
+}
