18bf7afdde2e3f9b38dec7c25637603e95ddc68d
윤주찬 <wncks2546@gmail.com>
2024-01-29 10:07:52 +0900

    Design: [FE] 내담자 상담화면, 프로필, 자가진단 그리드 설정

--- Frontend/malitell/src/routes/auth/profile.tsx
+++ Frontend/malitell/src/routes/auth/profile.tsx
@@ -5,20 +5,26 @@ export default function Profile() {
   return (
     <g.Back>
       <g.Grid>
-        <g.Box col="1/13" row="2/3">
+        <g.Box col="1/4" row="1/2">
           Profile
         </g.Box>
-        <g.Box col="1/4" row="3/6">
+        <g.Box col="1/4" row="2/5">
           profileImage
         </g.Box>
-        <g.Box col="1/4" row="6/7">
+        <g.Box col="1/4" row="5/6">
           tag
         </g.Box>
-        <g.Box col="1/4" row="7/12">
+        <g.Box col="1/4" row="6/9">
           profileMenu
         </g.Box>
-        <g.Box col="4/13" row="3/12">
-          profileDetail
+        <g.Box col="1/4" row="9/11">
+          metavers banner
+        </g.Box>
+        <g.Box col="4/13" row="1/2">
+          current tab
+        </g.Box>
+        <g.Box col="4/13" row="2/11">
+          current content
         </g.Box>
       </g.Grid>
     </g.Back>
