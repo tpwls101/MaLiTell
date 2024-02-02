51db545ebe39f233fe87b16f2b6095e9234c973c
뚱이 <djjang0925@gmail.com>
2024-02-02 16:47:57 +0900

    Refactor: 이메일 인증, 소켓 접속 확인

---
+++ Frontend/malitell/src/styles/auth/signup/emailCertification.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  width: 100%;
+  margin: 3% auto;
+  justify-content: space-around;
+  @media screen and (max-width: 1023px) {
+    margin: auto 0;
+    width: 100%;
+  }
+`;
