4336be2e4d7fdfd34d169d9873c1d97058ab1dd1
jaehyeon <hjaehyeon98@gmail.com>
2024-02-02 16:12:22 +0900

    Feat: 마음 자판기 기능 구현

--- Backend/Malitell/src/main/java/com/ssafy/malitell/Initializer.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/Initializer.java
@@ -1,7 +1,10 @@
 package com.ssafy.malitell;
 
 
+import com.ssafy.malitell.domain.capsule.Capsule;
 import com.ssafy.malitell.dto.request.user.ClientJoinRequestDto;
+import com.ssafy.malitell.repository.CapsuleRepository;
+import com.ssafy.malitell.service.CapsuleService;
 import com.ssafy.malitell.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
@@ -13,6 +16,7 @@ import org.springframework.stereotype.Component;
 public class Initializer implements ApplicationRunner {
 
     private final UserService userService;
+    private final CapsuleRepository capsuleRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -22,6 +26,15 @@ public class Initializer implements ApplicationRunner {
 
         userService.joinClient(joinDto1);
         userService.joinClient(joinDto2);
+
+        // 마음 자판기 내용물 추가
+        capsuleRepository.save(new Capsule("너는 세상에 도움이 되는 존재이다.", "https://www.youtube.com/shorts/QccXIRS9dsQ"));
+        capsuleRepository.save(new Capsule("(심리기획자 이명수: 내 마음이 지옥일 때)", "https://www.youtube.com/watch?v=r19P3I075pE"));
+        capsuleRepository.save(new Capsule("김창옥 Tv : 사는게 갑자기 힘들다면", "https://www.youtube.com/watch?v=ka2-P1OeDd8"));
+        capsuleRepository.save(new Capsule("힘들 때 힘을 빼면 힘이 생긴다", "https://www.youtube.com/watch?v=3Dn05-HrEaE"));
+        capsuleRepository.save(new Capsule("현실이 지치고 힘들 때 보면 위로가 되는 이야기들", "https://www.youtube.com/watch?v=9hpEEVRM9ao&t=5s"));
+        capsuleRepository.save(new Capsule("파이팅 해야지", "https://www.youtube.com/watch?v=vN0iy88IMLg"));
+        capsuleRepository.save(new Capsule("smile boy", "https://www.youtube.com/watch?v=45wpm7J9Se4"));
 
     }
 }
--- Backend/Malitell/src/main/java/com/ssafy/malitell/service/CapsuleService.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/service/CapsuleService.java
@@ -1,12 +1,26 @@
 package com.ssafy.malitell.service;
 
+import com.ssafy.malitell.domain.capsule.Capsule;
+import com.ssafy.malitell.dto.response.CapsuleResponseDto;
 import com.ssafy.malitell.repository.CapsuleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class CapsuleService {
 
     public final CapsuleRepository capsuleRepository;
+
+    public CapsuleResponseDto getContent() {
+        List<Capsule> allCapsule = capsuleRepository.findAll();
+
+        Random random = new Random();
+        Capsule capsule = allCapsule.get(random.nextInt(allCapsule.size()));
+
+        return new CapsuleResponseDto(capsule.getPhrases(), capsule.getVideoLink());
+    }
 }
