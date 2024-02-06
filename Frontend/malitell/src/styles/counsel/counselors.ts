fc309dedb255f847da02ac50ae2c0eeb7e4ebb85
윤주찬 <wncks2546@gmail.com>
2024-02-07 00:40:43 +0900

    Feat: 상담가 리스트 연결 작업

--- Frontend/malitell/src/styles/counsel/counselors.ts
+++ Frontend/malitell/src/styles/counsel/counselors.ts
@@ -8,3 +8,12 @@ export const Wrapper = styled.div`
   justify-content: end;
   flex-wrap: wrap;
 `
+
+export const Counselor = styled.div`
+width: 98%;
+height: 200px;
+margin-top: 1.5%;
+background-color: white;
+border-radius: 10px;
+box-shadow: 1.5px 1.5px 1.5px 1.5px lightgray;
+`
