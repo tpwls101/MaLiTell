fbd4a2fea6ee5567e53d45bb7265d843c82a9259
jaehyeon <hjaehyeon98@gmail.com>
2024-02-06 13:36:24 +0900

    Feat: 상담가 회원가입시 아이디가 중복인지 확인하는 코드 추가

--- Backend/Malitell/src/main/java/com/ssafy/malitell/Initializer.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/Initializer.java
@@ -4,6 +4,7 @@ package com.ssafy.malitell;
 import com.ssafy.malitell.domain.capsule.Capsule;
 import com.ssafy.malitell.domain.mindletgo.MindLetGoTopic;
 import com.ssafy.malitell.dto.request.user.ClientJoinRequestDto;
+import com.ssafy.malitell.dto.request.user.CounselorJoinRequestDto;
 import com.ssafy.malitell.repository.CapsuleRepository;
 import com.ssafy.malitell.repository.MindLetGoRepository;
 import com.ssafy.malitell.repository.MindLetGoTopicRepositoryImpl;
@@ -27,7 +28,9 @@ public class Initializer implements ApplicationRunner {
         // 유저 더미데이터 추가
         ClientJoinRequestDto joinDto1 = new ClientJoinRequestDto("hanjaehyeon", "한재현", "짱재현", "1234", "hjaehyeon98@gmail.com", "010-5705-6540", "19980228", "M", "ROLE_CLIENT");
         ClientJoinRequestDto joinDto2 = new ClientJoinRequestDto("tpwls101", "유세진", "짱세진", "1234", "tpwls101@naver.com", "010-6604-1442", "19980210", "F", "ROLE_CLIENT");
+        CounselorJoinRequestDto joinDto3 = new CounselorJoinRequestDto("wncks123", "윤주찬", "짱주찬", "1234", "wncks2546@naver.com", "010-5566-7679", "19990604", "M", 10, "ROLE_COUNSELOR");
 
+        userService.joinCounselor(joinDto3);
         userService.joinClient(joinDto1);
         userService.joinClient(joinDto2);
 
