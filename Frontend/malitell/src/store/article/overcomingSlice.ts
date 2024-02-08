78aff9fea755a79e86ab0a7b720fe9e8bb61005e
윤주찬 <wncks2546@gmail.com>
2024-02-08 14:52:54 +0900

    Feat: 게시글 태그기능

--- Frontend/malitell/src/store/article/overcomingSlice.ts
+++ Frontend/malitell/src/store/article/overcomingSlice.ts
@@ -1,10 +1,10 @@
-import { api, authApi } from "../axiosInstance"
+import { api } from "../axiosInstance"
 
 export const fetchOvercomingList = () => {
     const res = api.get('/overComing/getBoardList')
     .then((res) => {
         return res.data
     })
-    .catch((error) => console.log("load fail"))
+    .catch((error) => console.log("load fail", error))
     return res
 }
