77bf9b9153e30bf561699880c207f8fe51b8cfbf
뚱이 <djjang0925@gmail.com>
2024-02-10 19:12:59 +0900

    Refactor: 마음자판기 동작코드 완성

--- Frontend/malitell/src/styles/common/vendingMachine/card.ts
+++ Frontend/malitell/src/styles/common/vendingMachine/card.ts
@@ -1,23 +1,43 @@
-import styled from "styled-components";
+import front from "../../../assets/images/vendingMachine/cardFront.png";
+import back from "../../../assets/images/vendingMachine/cardBack.png";
+import styled, { css, keyframes } from "styled-components";
 
-export const Wrapper = styled.div`
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+export const Wrapper = styled.div<{
+  $display?: string;
+  $fadeOut?: string;
+  $position?: boolean;
+}>`
+  transition: 0.2s;
   width: 32%;
   height: 500px;
+  display: ${({ $display }) => $display};
   position: relative;
-  &:hover {
-    /* transform: rotateY(180deg); */
-  }
-  `;
 
-export const Card = styled.div<{$flipped?:boolean}>`
+  ${({ $fadeOut }) =>
+    $fadeOut === "none" &&
+    css`
+      animation: ${fadeOut} 1s;
+    `}
+`;
+
+export const Card = styled.div<{ $flipped?: boolean }>`
   width: 100%;
   height: 100%;
   position: relative;
   transition: 0.5s ease-in-out;
   transform-style: preserve-3d;
-  transform: ${({ $flipped }) => $flipped ? 'rotateY(180deg)' : 'rotateY(0deg)'};
+  transform: ${({ $flipped }) =>
+    $flipped ? "rotateY(180deg)" : "rotateY(0deg)"};
 `;
-
 
 export const Content = styled.div`
   position: absolute;
@@ -26,13 +46,24 @@ export const Content = styled.div`
   backface-visibility: hidden;
   display: flex;
   justify-content: center;
+  border-radius: 10px;
   align-items: center;
   color: #fff;
   &.front {
-    background-color: tomato;
+    background-image: url(${front});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    &:hover {
+      transform: scale(1.05);
+      transition: 0.2s;
+    }
   }
   &.back {
-    background-color: royalblue;
-    transform: rotateY(180deg);
+    background-image: url(${back});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    transform: scale(1.2) rotateY(180deg);
   }
 `;
