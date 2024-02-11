b1795e8935b27e810899de9b43da7f52ce6f8a86
kn9012 <kn9012@naver.com>
2024-02-11 18:17:53 +0900

    Update: StompHandler 수정

--- Backend/Malitell/src/main/java/com/ssafy/malitell/handler/StompHandler.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/handler/StompHandler.java
@@ -26,8 +26,6 @@ public class StompHandler implements ChannelInterceptor {
 
     private final JWTUtil jwtUtil;
     private final UserRepository userRepository;
-    private final ChatRoomRepository chatRoomRepository;
-    private final ChatService chatService;
 
     // websocket을 통해 들어온 요청이 처리 되기전 실행된다.
     @Override
@@ -41,8 +39,27 @@ public class StompHandler implements ChannelInterceptor {
             if (StringUtils.hasText(jwtToken) && jwtToken.startsWith("Bearer")) {
                 jwtToken = jwtToken.substring(6, jwtToken.length());
             }
-            String userId = jwtUtil.getUserId(jwtToken);
         }
         return message;
+    }
+
+    @Override
+    public void postSend(Message message, MessageChannel channel, boolean sent) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String sessionId = accessor.getSessionId();
+        switch (accessor.getCommand()) {
+            case CONNECT:
+                // 유저가 Websocket으로 connect()를 한 뒤 호출됨
+                break;
+            case DISCONNECT:
+                log.info("DISCONNECT");
+                log.info("sessionId: {}",sessionId);
+                log.info("channel:{}",channel);
+                // 유저가 Websocket으로 disconnect() 를 한 뒤 호출됨 or 세션이 끊어졌을 때 발생함(페이지 이동~ 브라우저 닫기 등)
+                break;
+            default:
+                break;
+        }
+
     }
 }
