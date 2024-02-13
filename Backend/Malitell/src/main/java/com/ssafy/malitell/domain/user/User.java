f6358650bc23910a5dae1f99e415e1442f31d7d5
jaehyeon <hjaehyeon98@gmail.com>
2024-02-13 17:20:30 +0900

    Fix: 회원가입시 readChk에 문제가 생기던 문제 해결

--- Backend/Malitell/src/main/java/com/ssafy/malitell/domain/user/User.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/domain/user/User.java
@@ -9,6 +9,7 @@ import com.ssafy.malitell.dto.request.user.ClientUpdateRequestDto;
 import com.ssafy.malitell.dto.request.user.CounselorUpdateRequestDto;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
