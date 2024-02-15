c10fc0fb56694bc40fe00d2b0e90231d7b942e3e
kn9012 <kn9012@naver.com>
2024-02-15 10:44:15 +0900

    Update: Socket url 수정

--- Backend/Malitell/src/main/java/com/ssafy/malitell/config/WebSocketConfig.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/config/WebSocketConfig.java
@@ -17,13 +17,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/sub");
-        registry.setApplicationDestinationPrefixes("/pub");
+        registry.enableSimpleBroker("/api/sub");
+        registry.setApplicationDestinationPrefixes("/api/pub");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/api/ws-stomp").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
--- Backend/Malitell/src/main/java/com/ssafy/malitell/service/chat/RedisSubscriber.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/service/chat/RedisSubscriber.java
@@ -31,7 +31,7 @@ public class RedisSubscriber implements MessageListener {
             ChatMessageDto roomMessage = objectMapper.readValue(publishMessage, ChatMessageDto.class);
 
             // 5. Websocket 구독자에게 채팅 메시지 전송
-            messagingTemplate.convertAndSend("/sub/chat/room/" + roomMessage.getChatRoomSeq(), roomMessage);
+            messagingTemplate.convertAndSend("/api/sub/chat/room/" + roomMessage.getChatRoomSeq(), roomMessage);
         } catch (Exception e) {
             log.error(e.getMessage());
         }
@@ -45,7 +45,7 @@ public class RedisSubscriber implements MessageListener {
             // ChatMessage 객채로 맵핑
             ChatMessageResponseDto chatMessageResponseDto = objectMapper.readValue(publishMessage, ChatMessageResponseDto.class);
             // 채팅방을 구독한 클라이언트에게 메시지 발송
-            messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessageResponseDto.getChatRoomSeq(), chatMessageResponseDto);
+            messagingTemplate.convertAndSend("/api/sub/chat/room/" + chatMessageResponseDto.getChatRoomSeq(), chatMessageResponseDto);
         } catch (Exception e) {
             log.error("Exception {}", e);
         }
