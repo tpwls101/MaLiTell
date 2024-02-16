3844fe35cde4eeaf1e801843a2d6ef08ac49f48e
뚱이 <djjang0925@gmail.com>
2024-02-16 13:38:53 +0900

    temp

--- Backend/Malitell/src/main/java/com/ssafy/malitell/jwt/LoginFilter.java
+++ Backend/Malitell/src/main/java/com/ssafy/malitell/jwt/LoginFilter.java
@@ -48,45 +48,45 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
         // token에 담은 데이터를 검증을 위해 AuthenticationManager로 전달
         return authenticationManager.authenticate(authToken);
+        }
+
+        @Override
+        protected void successfulAuthentication (HttpServletRequest request, HttpServletResponse response, FilterChain
+        chain, Authentication authentication) throws IOException, ServletException {
+            // 로그인 성공 -> 로그인한 유저의 정보를 가지고 JWT 발급
+
+            // getPrincipal() : 현재 사용자 정보 가져오기
+
+            CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+
+            String userId = customUserDetails.getUsername(); // 아이디
+
+            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+            Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+            GrantedAuthority auth = iterator.next();
+            String role = auth.getAuthority(); // 권한
+
+            // 사용자 아이디, 권한 입력해서 JWT 발급
+            String accessToken = jwtUtil.createAccessToken(userId, role); // access token
+            String refreshToken = jwtUtil.createRefreshToken(userId, role); // refresh token
+
+            // Bearer 인증 방식
+            // 응답 헤더에 JWT 토큰 값을 넣어 응답
+            // 응답 헤더에서 "Authorization" key 값에 "Bearer ~~" 값을 확인할 수 있음 (JWT)
+            response.addHeader(JWTUtil.ACCESS_TOKEN, "Bearer " + accessToken);
+            response.addHeader(JWTUtil.REFRESH_TOKEN, "Bearer " + refreshToken);
+
+            // 디비에 refreshToken 저장
+            userRepository.updateRefreshToken(userId, refreshToken);
+        }
+
+        // 로그인 실패시 실행하는 메소드
+        @Override
+        protected void unsuccessfulAuthentication (HttpServletRequest request, HttpServletResponse
+        response, AuthenticationException failed) throws IOException, ServletException {
+            // 로그인 실패 시 401 응답 (Unauthorized)
+            response.setStatus(401);
+        }
+
     }
-
-    @Override
-    protected void successfulAuthentication (HttpServletRequest request, HttpServletResponse response, FilterChain
-            chain, Authentication authentication) throws IOException, ServletException {
-        // 로그인 성공 -> 로그인한 유저의 정보를 가지고 JWT 발급
-
-        // getPrincipal() : 현재 사용자 정보 가져오기
-
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-
-        String userId = customUserDetails.getUsername(); // 아이디
-
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
-        GrantedAuthority auth = iterator.next();
-        String role = auth.getAuthority(); // 권한
-
-        // 사용자 아이디, 권한 입력해서 JWT 발급
-        String accessToken = jwtUtil.createAccessToken(userId, role); // access token
-        String refreshToken = jwtUtil.createRefreshToken(userId, role); // refresh token
-
-        // Bearer 인증 방식
-        // 응답 헤더에 JWT 토큰 값을 넣어 응답
-        // 응답 헤더에서 "Authorization" key 값에 "Bearer ~~" 값을 확인할 수 있음 (JWT)
-        response.addHeader(JWTUtil.ACCESS_TOKEN, "Bearer " + accessToken);
-        response.addHeader(JWTUtil.REFRESH_TOKEN, "Bearer " + refreshToken);
-
-        // 디비에 refreshToken 저장
-        userRepository.updateRefreshToken(userId, refreshToken);
-    }
-
-    // 로그인 실패시 실행하는 메소드
-    @Override
-    protected void unsuccessfulAuthentication (HttpServletRequest request, HttpServletResponse
-            response, AuthenticationException failed) throws IOException, ServletException {
-        // 로그인 실패 시 401 응답 (Unauthorized)
-        response.setStatus(401);
-    }
-
-}
 
